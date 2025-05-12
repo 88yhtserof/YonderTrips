87b3d7ed28ac87927a255cf41d383ab234b76056
88yhtserof <88yhtserofg@gmail.com>
2025-05-12 20:28:11 +0900

    Create project YonderTrips

---
+++ YonderTrips/YonderTrips/YonderTripsApp.swift
@@ -0,0 +1,17 @@
+//
+//  YonderTripsApp.swift
+//  YonderTrips
+//
+//  Created by 임윤휘 on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct YonderTripsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
