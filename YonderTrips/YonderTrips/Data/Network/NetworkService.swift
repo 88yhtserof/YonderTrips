cd8796dc1180eb2c821d333bdb2ad7b1acb457dd
88yhtserof <88yhtserofg@gmail.com>
2025-05-14 20:46:17 +0900

    [FEAT] NetworkService 구현 #1

---
+++ YonderTrips/YonderTrips/Data/Network/NetworkService.swift
@@ -0,0 +1,87 @@
+//
+//  NetworkService.swift
+//  YonderTrips-Practice
+//
+//  Created by 임윤휘 on 5/9/25.
+//
+
+import SwiftUI
+
+struct NetworkService {
+    
+    private let logger: YonderTripsLogger
+    private let session: URLSession
+    
+    init(logger: YonderTripsLogger, session: URLSession = .shared) {
+        self.logger = logger
+        self.session = session
+    }
+    
+    func request<T, Config>(apiConfiguration api: Config) async throws -> T where T : Decodable, Config : APIConfiguration & APIErrorConvertible {
+        
+        guard let url = api.url else {
+            
+            let error = ClientError.invalidURL(api.url?.absoluteString)
+            YonderTripsLogger.shared.error(error)
+            throw error
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = api.method
+        request.allHTTPHeaderFields = api.headers
+        request.httpBody = api.body
+        
+        var data: Data
+        var response: URLResponse
+        
+        do {
+            let result = try await URLSession.shared.data(for: request)
+            (data, response) = (result.0, result.1)
+            
+        } catch {
+            let error = ClientError.requestCreationFailed(error)
+            YonderTripsLogger.shared.error(error)
+            throw error
+        }
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            
+            let error = ClientError.missingResponseData
+            YonderTripsLogger.shared.error(error)
+            throw error
+        }
+        
+        guard (200..<300) ~= httpResponse.statusCode else {
+            let error = api.error(data, statusCode: httpResponse.statusCode)
+            YonderTripsLogger.shared.error(error)
+            throw error
+        }
+        logger.debug("Success: request \(api)")
+        
+        do {
+            let decoded = try JSONDecoder().decode(T.self, from: data)
+            return decoded
+        } catch {
+            let error = ClientError.failedDecoding(error)
+            YonderTripsLogger.shared.error(error)
+            throw error
+        }
+    }
+    
+}
+
+extension NetworkService: EnvironmentKey {
+    
+    static let defaultValue: NetworkService = {
+        return NetworkService(logger: YonderTripsLogger.shared)
+    }()
+}
+
+extension EnvironmentValues {
+    
+    var networkService: NetworkService {
+        get { self[NetworkService.self] }
+        set { self[NetworkService.self] = newValue }
+    }
+}
+
