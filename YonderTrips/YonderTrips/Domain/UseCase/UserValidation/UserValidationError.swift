6160c7a33e9624e7af0621bb82ef0b19476d1c85
88yhtserof <88yhtserofg@gmail.com>
2025-05-16 18:01:18 +0900

    [FEAT] UserValidation: 사용자 정보 유효성 검사 로직 구현 #3

---
+++ YonderTrips/YonderTrips/Domain/UseCase/UserValidation/UserValidationError.swift
@@ -0,0 +1,25 @@
+//
+//  UserValidationError.swift
+//  YonderTrips
+//
+//  Created by 임윤휘 on 5/16/25.
+//
+
+import Foundation
+
+enum UserValidationError: Error, LocalizedError {
+    case invalidEmail
+    case invalidNick
+    case invalidPassword
+    
+    var errorDescription: String? {
+        switch self {
+        case .invalidEmail:
+            return "유효하지 않은 이메일 형식입니다. 올바른 이메일 주소를 입력해주세요."
+        case .invalidNick:
+            return "닉네임에는 다음과 같은 문자를 포함할 수 없습니다.\n. , ? * - @ "
+        case .invalidPassword:
+            return "비밀번호는 최소 8자 이상이어야 하며, 영문자, 숫자, 특수문자(@$!%*#?&)를 각각 최소 1개 이상 포함해야 합니다."
+        }
+    }
+}
---
+++ YonderTrips/YonderTrips/Domain/UseCase/UserValidation/UserValidationUseCase.swift
@@ -0,0 +1,68 @@
+//
+//  UserValidationUseCase.swift
+//  YonderTrips
+//
+//  Created by 임윤휘 on 5/16/25.
+//
+
+import Foundation
+
+struct UserValidationUseCase {
+    
+    enum ValidationType {
+        case email(String)
+        case nick(String)
+        case password(String)
+        
+        var message: String {
+            switch self {
+            case .email:
+                return "가입 가능한 이메일입니다."
+            case .nick:
+                return "사용할 수 있는 닉네임입니다."
+            case .password:
+                return "사용 가능한 비밀번호입니다."
+            }
+        }
+    }
+    
+    func validateUserInfo(_ type: ValidationType) throws -> String {
+        
+        switch type {
+            
+        case .email(let value):
+            guard validateEmail(value) else {
+                throw UserValidationError.invalidEmail
+            }
+        case .nick(let value):
+            guard validateNick(value) else {
+                throw UserValidationError.invalidNick
+            }
+            
+        case .password(let value):
+            guard validatePassword(value) else {
+                throw UserValidationError.invalidPassword
+            }
+        }
+        
+        return type.message
+    }
+}
+
+private extension UserValidationUseCase {
+    
+    func validateEmail(_ email: String) -> Bool {
+        let pattern = #"^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"#
+        return email.range(of: pattern, options: .regularExpression) != nil
+    }
+    
+    func validateNick(_ nick: String) -> Bool {
+        let forbiddenCharacters = CharacterSet(charactersIn: ".,?*-@")
+        return nick.rangeOfCharacter(from: forbiddenCharacters) == nil
+    }
+    
+    func validatePassword(_ password: String) -> Bool {
+        let pattern = #"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"#
+        return password.range(of: pattern, options: .regularExpression) != nil
+    }
+}
