970b748e7929d95ced147ca5fcd78546b7ed0a5a
88yhtserof <88yhtserofg@gmail.com>
2025-05-20 16:07:13 +0900

    [FEAT] RootFlowRouter: 화면 플로우 로직에 따른 루트 뷰 switching을 위한 Router 구현 #3

---
+++ YonderTrips/YonderTrips/Presentation/RootFlowRouter.swift
@@ -0,0 +1,19 @@
+//
+//  RootFlowRouter.swift
+//  YonderTrips
+//
+//  Created by 임윤휘 on 5/20/25.
+//
+
+import SwiftUI
+
+@MainActor
+final class RootFlowRouter: ObservableObject {
+    
+    @Published var rootFlow: RootFlow = .signUp
+    
+    enum RootFlow: Hashable {
+        case signIn
+        case signUp
+    }
+}
