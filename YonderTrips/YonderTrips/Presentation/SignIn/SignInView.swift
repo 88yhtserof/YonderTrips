ec5ac80cca09ccf77e7556fc7736b532bc70a39a
88yhtserof <88yhtserofg@gmail.com>
2025-05-21 19:17:59 +0900

    [FEAT] SignInFlowRouter 구현 및 로그인 화면에서 이메일 로그인 버튼 선택 시 화면 전환 #7

--- YonderTrips/YonderTrips/Presentation/SignIn/SignInView.swift
+++ YonderTrips/YonderTrips/Presentation/SignIn/SignInView.swift
@@ -14,80 +14,112 @@ import KakaoSDKUser
 
 struct SignInView: View {
     
+    @Environment(\.container) private var container
     @Environment(\.networkService) private var networkService
+    @StateObject private var signInRouter = SignInFlowRouter()
     
     var body: some View {
-        VStack(alignment: .leading) {
-            
-            Text("YonderTrips.")
-                .lineSpacing(8)
-                .font(Font.yt(.paperlogy(.caption2)))
-                .foregroundColor(.gray15)
-                .padding(.vertical, 4)
-                .padding(.horizontal, 12)
-                .background(.blackSeafoam)
-                .clipShape(RoundedRectangle(cornerRadius: 3))
-                .padding(.top, 55)
-                .padding(.leading, 16)
-            
-            Text("Your Trips,\nYonder Bound")
-                .lineSpacing(6)
-                .font(Font.yt(.paperlogy(.slogan1)))
-                .foregroundColor(.gray0)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.top, 8)
-                .padding(.leading, 16)
-            
-            Text("저 너머 당신의 멋진 여행을 위해")
-                .font(Font.yt(.paperlogy(.slogan2)))
-                .foregroundColor(.gray30)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.top, 1)
-                .padding(.leading, 16)
-            
-            Spacer()
-            
-            VStack(alignment: .center, spacing: 8) {
+        NavigationStack(path: $signInRouter.path) {
+            VStack(alignment: .leading) {
                 
-                SignInWithAppleButton(.signIn) { request in
-                    request.requestedScopes = [.fullName, .email]
-                } onCompletion: { result in
-                    appleLoginButtonAction(result)
+                Text("YonderTrips.")
+                    .lineSpacing(8)
+                    .font(Font.yt(.paperlogy(.caption2)))
+                    .foregroundColor(.gray15)
+                    .padding(.vertical, 4)
+                    .padding(.horizontal, 12)
+                    .background(.blackSeafoam)
+                    .clipShape(RoundedRectangle(cornerRadius: 3))
+                    .padding(.top, 55)
+                    .padding(.leading, 16)
+                
+                Text("Your Trips,\nYonder Bound")
+                    .lineSpacing(6)
+                    .font(Font.yt(.paperlogy(.slogan1)))
+                    .foregroundColor(.gray0)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.top, 8)
+                    .padding(.leading, 16)
+                
+                Text("저 너머 당신의 멋진 여행을 위해")
+                    .font(Font.yt(.paperlogy(.slogan2)))
+                    .foregroundColor(.gray30)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.top, 1)
+                    .padding(.leading, 16)
+                
+                Spacer()
+                
+                VStack(alignment: .center, spacing: 8) {
+                    
+                    // email signIn
+                    Button(action: emailLoginButtonAction) {
+                        Text("이메일 로그인 • 회원가입")
+                            .font(Font.yt(.pretendard(.body1)))
+                            .frame(width: 300, height: 45)
+                            .background(.gray0)
+                            .foregroundColor(.gray90)
+                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                    }
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.gray45, lineWidth: 1)
+                    }
+                    
+                    // kakao signIn
+                    Button(action: kakaoLoginButtonAction) {
+                        Image("kakao_login_large_wide")
+                            .resizable()
+                            .frame(width: 300, height: 45)
+                    }
+                    
+                    // apple signIn
+                    SignInWithAppleButton(.signIn) { request in
+                        request.requestedScopes = [.fullName, .email]
+                    } onCompletion: { result in
+                        appleLoginButtonAction(result)
+                    }
+                    .frame(width: 300, height: 45)
+                    .signInWithAppleButtonStyle(.black)
                 }
-                .frame(width: 300, height: 45)
-                .signInWithAppleButtonStyle(.black)
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 16)
                 
-                Button(action: kakaoLoginButtonAction) {
-                    Image("kakao_login_large_wide")
+            }
+            .background {
+                ZStack {
+                    Image("TripsPoster")
                         .resizable()
-                        .frame(width: 300, height: 45)
+                        .aspectRatio(contentMode: .fill)
+                        .ignoresSafeArea()
+                    
+                    Color.gray90.opacity(0.38)
+                        .ignoresSafeArea()
+                    
+                    
+                }
+                .frame(maxWidth: .infinity)
+            }
+            .navigationDestination(for: SignInFlowRouter.SignInFlow.self) { flow in
+                
+                switch flow {
+                case .emailSignIn:
+                    EmailSignInView()
+                case .signUp:
+                    SignUpView(viewModel: container.makeSignUpViewModel())
                 }
             }
-            .frame(maxWidth: .infinity)
-            .padding(.vertical, 16)
-            
         }
-        .background {
-            ZStack {
-                Image("TripsPoster")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .ignoresSafeArea()
-                
-                Color.gray90.opacity(0.38)
-                    .ignoresSafeArea()
-                
-                
-            }
-            .frame(maxWidth: .infinity)
-        }
-        
     }
 }
 
 //MARK: - Action
 extension SignInView {
-
+    
+    func emailLoginButtonAction() {
+        signInRouter.path.append(SignInFlowRouter.SignInFlow.emailSignIn)
+        
+    }
     
     func appleLoginButtonAction(_ result: Result<ASAuthorization, Error>) {
         
