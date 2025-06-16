4b3c0cafdfac90f130bb6e415b441dd9dd8579c9
88yhtserof <88yhtserofg@gmail.com>
2025-06-16 11:24:47 +0900

    [CHORE] 기능 구현 확인을 위한 임시 UI 구성 #28

---
+++ YonderTrips/YonderTrips/Presentation/OrderTestView.swift
@@ -0,0 +1,24 @@
+//
+//  OrderTestView.swift
+//  YonderTrips
+//
+//  Created by 임윤휘 on 6/16/25.
+//
+
+import SwiftUI
+
+struct OrderTestView: View {
+    
+    var body: some View {
+        Button {
+            print("주문 생성")
+        } label: {
+            Text("예약하기")
+        }
+
+    }
+}
+
+#Preview {
+    OrderTestView()
+}
