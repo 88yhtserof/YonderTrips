59b1718d63c4dbe891a652c74694f822bb9ee97b
88yhtserof <88yhtserofg@gmail.com>
2025-06-23 14:30:04 +0900

    [FEAT] ActivityPost List 구성 #36

---
+++ YonderTrips/YonderTrips/Presentation/ActivityPostList/ActivityPostListView.swift
@@ -0,0 +1,31 @@
+//
+//  ActivityPostListView.swift
+//  YonderTrips
+//
+//  Created by 임윤휘 on 6/23/25.
+//
+
+import SwiftUI
+
+struct ActivityPostListView: View {
+    
+    private let postSummaryList = PostSummary.dummyData
+    
+    var body: some View {
+        
+        ScrollView {
+            VStack {
+                ForEach(postSummaryList, id: \.postId) { item in
+                    ActivityPostCellView(postSummary: item)
+                    
+                    VerticalDivider()
+                }
+            }
+        }
+        .background(.gray0)
+    }
+}
+
+#Preview {
+    ActivityPostListView()
+}
