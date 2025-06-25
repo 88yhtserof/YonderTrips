38df80fcf1309da6ffc4852beb31703428fc76e2
88yhtserof <88yhtserofg@gmail.com>
2025-06-25 19:02:24 +0900

    [FEAT] 빈 화면 대응 #36

--- YonderTrips/YonderTrips/Presentation/ActivityPostList/ActivityPostListView.swift
+++ YonderTrips/YonderTrips/Presentation/ActivityPostList/ActivityPostListView.swift
@@ -19,11 +19,17 @@ struct ActivityPostListView: View {
                 CustomSlider()
                     .padding(16)
                 
-                VStack {
-                    ForEach(viewModel.state.postSummaryList, id: \.postId) { item in
-                        ActivityPostCellView(postSummary: item)
-                        
-                        VerticalDivider()
+                if viewModel.state.postSummaryList.isEmpty {
+                    Text("아직 게시된 글이 없습니다.")
+                        .font(.yt(.pretendard(.body1)))
+                        .foregroundStyle(.gray60)
+                } else {
+                    VStack {
+                        ForEach(viewModel.state.postSummaryList, id: \.postId) { item in
+                            ActivityPostCellView(postSummary: item)
+                            
+                            VerticalDivider()
+                        }
                     }
                 }
             }
